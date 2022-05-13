eb427f60cea18580554e9adfa75e9eb97cb412cb
ECEM <ecemozturk704@gmail.com>
2022-05-13 13:20:36 +0300

    first inital

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -214,13 +214,14 @@
                       <img
                         class="w-10 h-10 rounded-full"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        
                         alt=""
                       />
                     </div>
 
                     <div class="ml-4">
                       <div class="text-sm font-medium leading-5 text-gray-900">
-                        {{ u.name }}
+                        {{ u }}
                       </div>
                       <div class="text-sm leading-5 text-gray-500">
                         {{ u.email }}
@@ -344,8 +345,9 @@ interface User {
   role: string;
 }
 
+
 const testUser: User = {
-  name: "John Doe",
+  name: "Ecem Doe",
   email: "john@example.com",
   title: "Software Engineer",
   title2: "Web dev",
