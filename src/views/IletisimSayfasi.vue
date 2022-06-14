0eea7dc058225fc067a8417ba57c23faca201a71
ECEM <ecemozturk704@gmail.com>
2022-06-14 16:14:52 +0300

    Iletisim sayfasi frontend degistirildi

--- src/views/IletisimSayfasi.vue
+++ src/views/IletisimSayfasi.vue
@@ -1,46 +1,73 @@
-<!--<template>
+<template>
   <div>
+    <!-- Breadcrumb -->
+    <!-- <Breadcrumb breadcrumb="Tables" /> -->
+
+    <div class="mt-4">
+      <h4 class="text-black-600">KATEGORILER</h4>
+
+      <div class="mt-6">
+        <div class="my-6 overflow-hidden bg-white rounded-md shadow">
+          <table class="w-full text-left border-collapse">
+            <thead class="border-b">
+              <tr>
+                <th
+                  class="px-5 py-3 text-sm font-medium text-gray-100 uppercase bg-indigo-800"
+                >
+                  Kategori ID
+                </th>
+                <th
+                  class="px-5 py-3 text-sm font-medium text-gray-100 uppercase bg-indigo-800"
+                >
+                  Kategori Adi
+                </th><th
+                  class="px-5 py-3 text-sm font-medium text-gray-100 uppercase bg-indigo-800"
+                >
+                  Kategorinin Urunleri
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="category in categories" :key="category.categoryId"
+                class="hover:bg-gray-200"
+              >
+                <td class="px-6 py-4 text-lg text-gray-700 border-b">
+                  {{ category.categoryId }}
+                </td>
+                <td class="px-6 py-4 text-gray-500 border-b">
+                  {{ category.categoryName }}
+                </td>
+                <td class="px-6 py-4 text-gray-500 border-b">
+                  {{ category.products }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+   
+
     
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Image</th>
-          <th scope="col">Stock</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="product in products" :key="product.productId">
-          <th scope="row">{{ product.productId }}</th>
-          <td>{{ product.productName }}</td>
-          <td>{{ product.productImage }}</td>
-          <td>{{ product.stock }}</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import axios from "axios";
+import { useTableData } from '../hooks/useTableData'
+const { simpleTableData, paginatedTableData, wideTableData } = useTableData()
 
-// import Breadcrumb from "../partials/Breadcrumb.vue";
-
-const products = ref([]);
+//import Breadcrumb from '../partials/Breadcrumb.vue'
+const categories = ref([]);
 onMounted(async () => {
   await axios
-    .get("http://kozmosapi-001-site1.itempurl.com/api/Products")
+    .get("http://kozmosapi-001-site1.itempurl.com/api/Categories")
     .then((response) => {
-      products.value = response.data;
+      categories.value = response.data;
     });
-  console.log(products);
+  console.log(categories);
 });
-
-//composition API
-
-const open = ref(false);
-</script> 
--->
-
+</script>
