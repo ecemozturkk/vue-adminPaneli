15a53466b86b9bd2b4a26d365b3ceb39801019a9
ECEM <ecemozturk704@gmail.com>
2022-06-16 10:52:29 +0300

    Iletisim sayfasinda get customer  message gerceklestirildi

--- src/views/IletisimSayfasi.vue
+++ src/views/IletisimSayfasi.vue
@@ -4,7 +4,7 @@
     <!-- <Breadcrumb breadcrumb="Tables" /> -->
 
     <div class="mt-4">
-      <h4 class="text-black-600">KATEGORILER</h4>
+      <h4 class="text-black-600">KULLANICI MESAJLARI</h4>
 
       <div class="mt-6">
         <div class="my-6 overflow-hidden bg-white rounded-md shadow">
@@ -14,32 +14,32 @@
                 <th
                   class="px-5 py-3 text-sm font-medium text-gray-100 uppercase bg-indigo-800"
                 >
-                  Kategori ID
+                  Mesaj Başlığı
                 </th>
                 <th
                   class="px-5 py-3 text-sm font-medium text-gray-100 uppercase bg-indigo-800"
                 >
-                  Kategori Adi
+                  Mesaj İçeriği
                 </th><th
                   class="px-5 py-3 text-sm font-medium text-gray-100 uppercase bg-indigo-800"
                 >
-                  Kategorinin Urunleri
+                  Çözülme Durumu
                 </th>
               </tr>
             </thead>
             <tbody>
               <tr
-                v-for="category in categories" :key="category.categoryId"
+                v-for="message in messages" :key="message.title"
                 class="hover:bg-gray-200"
               >
                 <td class="px-6 py-4 text-lg text-gray-700 border-b">
-                  {{ category.categoryId }}
+                  {{ message.title }}
                 </td>
                 <td class="px-6 py-4 text-gray-500 border-b">
-                  {{ category.categoryName }}
+                  {{ message.text }}
                 </td>
                 <td class="px-6 py-4 text-gray-500 border-b">
-                  {{ category.products }}
+                  {{ message.isResolved }}
                 </td>
               </tr>
             </tbody>
@@ -61,13 +61,13 @@ import { useTableData } from '../hooks/useTableData'
 const { simpleTableData, paginatedTableData, wideTableData } = useTableData()
 
 //import Breadcrumb from '../partials/Breadcrumb.vue'
-const categories = ref([]);
+const messages = ref([]);
 onMounted(async () => {
   await axios
-    .get("http://kozmosapi-001-site1.itempurl.com/api/Categories")
+    .get("http://kozmosapi-001-site1.itempurl.com/api/CustomerMessage")
     .then((response) => {
-      categories.value = response.data;
+      messages.value = response.data;
     });
-  console.log(categories);
+  console.log(messages);
 });
 </script>
